61d789443c4667191ee67b27fc0924db2991e81f
Daniel Singh <singhdanield@gmail.com>
2025-07-21 21:49:41 +0200

    add overlapping windows to iterate through the signal

--- main.go
+++ main.go
@@ -15,10 +15,11 @@ func main() {
 	for i := 0; i < N; i++ {
 		t := float64(i) / FrequencyRate
 		signal := math.Cos(2*PI*426*t) + math.Cos(2*PI*200.5*t) + math.Sin(2*PI*100.20*t) + math.Sin(2*PI*10.56*t) + math.Sin(2*PI*400*t)
+		//signal := math.Cos(2*PI*400*t) + math.Cos(2*PI*200*t) + math.Sin(2*PI*100*t) + math.Sin(2*PI*10*t) + math.Sin(2*PI*400*t)
 		samples[i] = signal
 	}
 
-	frequencies := generateSpectogram(samples)
+	frequencies := generateSpectogram(samples)[0]
 
 	//frequenices
 	for i := 0; i < N/2; i++ {
--- spectogram.go
+++ spectogram.go
@@ -1,27 +1,45 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
 const windowSize = 1024
-const hopSize = 1024
+const hopSize = 512
 
-func generateSpectogram(samples []float64) []complex128 {
+func generateSpectogram(samples []float64) [][]complex128 {
 	N := len(samples)
-
 	if N == 0 {
-		return []complex128{}
+		return [][]complex128{}
 	}
 
-	//apply hamming window
-	for n := 0; n < N; n++ {
-		samples[n] *= 0.54 - 0.46*math.Cos(2*PI*float64(n)/float64(N-1))
+	windowCount := int(math.Ceil(float64(N-windowSize)/hopSize)) + 1
+
+	freqMatrix := make([][]complex128, windowCount)
+
+	//process the signal using overlapping windows
+	for i := 0; i < windowCount; i++ {
+		freqMatrix[i] = make([]complex128, windowSize)
+		start := i * hopSize
+		end := start + windowSize
+
+		if end > N {
+			end = N
+		}
+
+		curSamples := make([]float64, windowSize)
+		copy(curSamples, samples[start:end])
+
+		//apply hamming window
+		for n := 0; n < windowSize; n++ {
+			curSamples[n] *= 0.54 - 0.46*math.Cos(2*PI*float64(n)/float64(windowSize-1))
+		}
+
+		//inplace fft
+		fft(curSamples, windowSize, freqMatrix[i], 0, 0, 1)
+
+		//frequencies := fft2(curSamples, windowSize)
+		//frequencies := dft(curSamples)
 	}
-
-	//frequencies := fft2(samples, len(samples))
-	//frequencies := dft(samples)
-	frequencies := make([]complex128, len(samples))
-	fft(samples, len(samples), frequencies, 0, 0, 1)
-
-	return frequencies
-
+	return freqMatrix
 }
