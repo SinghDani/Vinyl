f8bf80dc813e4873c80a7184aadc9235fb7a7029
Daniel Singh <singhdanield@gmail.com>
2025-08-23 10:33:40 +0200

    add spectogram image generator function

--- spectogram.go
+++ spectogram.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"image"
+	"image/color"
+	"image/png"
+	"log"
 	"math"
+	"math/cmplx"
+	"os"
 )
 
 const windowSize = 1024
@@ -42,9 +48,51 @@ func generateSpectogram(samples []float64) [][]complex128 {
 		//freqMatrix[i] = fft2(curSamples, windowSize)
 		//freqMatrix[i] = dft(curSamples)
 	}
+
 	//remove second half of frequncies since they are exact mirrors of first half
 	for i := 0; i < windowCount; i++ {
 		freqMatrix[i] = freqMatrix[i][:windowSize/2]
 	}
 	return freqMatrix
+}
+
+func spectogramImage(freqMatrix [][]complex128) {
+	width := len(freqMatrix)
+	if width == 0 {
+		return
+	}
+	height := len(freqMatrix[0])
+	if height == 0 {
+		return
+	}
+
+	img := image.NewGray(image.Rect(0, 0, width, height))
+
+	//find the max magnitude for normalisation
+	maxMagnitude := 0.0
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			mag := cmplx.Abs(freqMatrix[i][j])
+			if mag > maxMagnitude {
+				maxMagnitude = mag
+			}
+		}
+	}
+
+	//lower frequencies are written to the bottom, higher ones to the top
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			mag := cmplx.Abs(freqMatrix[x][y])
+			value := (mag / maxMagnitude) * 255
+			img.SetGray(x, height-1-y, color.Gray{uint8(value)})
+		}
+	}
+
+	f, err := os.Create("spectogram-image.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	png.Encode(f, img)
 }
