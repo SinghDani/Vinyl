8614210cc211d01ad7ed66d0fd9426ac8b7a408d
Daniel Singh <singhdanield@gmail.com>
2025-08-25 17:36:01 +0200

    add function to extract the wav header

--- wavParsing.go
+++ wavParsing.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/binary"
+	"fmt"
+)
+
 type WavHeader struct {
 	ChunkID       string
 	Format        string
@@ -18,6 +23,43 @@ type WavHeader struct {
 
 type IncorrectWavFormat struct{}
 
-func (I *IncorrectWavFormat) Error() string {
-	return "Incorrect wav format"
+func (I IncorrectWavFormat) Error() string {
+	return "Incorrect Wav Format"
+}
+
+func ExtractWavHeader(headerData []byte) (*WavHeader, error) {
+	if len(headerData) != 44 {
+		return nil, IncorrectWavFormat{}
+	}
+
+	header := &WavHeader{}
+
+	header.ChunkID = string(headerData[:4])
+	if header.ChunkID != "RIFF" {
+		return nil, IncorrectWavFormat{}
+	}
+	header.ChunkSize = binary.LittleEndian.Uint32(headerData[4:8])
+	header.Format = string(headerData[8:12])
+	if header.Format != "WAVE" {
+		return nil, IncorrectWavFormat{}
+	}
+	header.Subchunk1ID = string(headerData[12:16])
+	if header.Subchunk1ID != "fmt " {
+		return nil, IncorrectWavFormat{}
+	}
+	header.Subchunk1Size = binary.LittleEndian.Uint32(headerData[16:20])
+	header.AudioFormat = binary.LittleEndian.Uint16(headerData[20:22])
+	header.NumChannels = binary.LittleEndian.Uint16(headerData[22:24])
+	header.SampleRate = binary.LittleEndian.Uint32(headerData[24:28])
+	header.ByteRate = binary.LittleEndian.Uint32(headerData[28:32])
+	header.BlockAlign = binary.LittleEndian.Uint16(headerData[32:34])
+	header.BitsPerSample = binary.LittleEndian.Uint16(headerData[34:36])
+	header.Subchunk2ID = string(headerData[36:40])
+	if header.Subchunk2ID != "data" {
+		return nil, IncorrectWavFormat{}
+	}
+	header.Subchunk2Size = binary.LittleEndian.Uint32(headerData[40:44])
+
+	fmt.Printf("%+v", header)
+	return header, nil
 }
