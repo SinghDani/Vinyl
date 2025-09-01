3a20a558e998c78e2e596084ed1ce2c2f51dac69
Daniel Singh <singhdanield@gmail.com>
2025-09-01 17:25:16 +0200

    add comments for clarity + bounds checking to the wav header parsing function

--- wavParsing.go
+++ wavParsing.go
@@ -30,23 +30,26 @@ func (I IncorrectWavFormat) Error() string {
 }
 
 func ParseWav(path string) error {
-	samples, err := os.ReadFile(path)
+	data, err := os.ReadFile(path)
 	if err != nil {
 		return err
 	}
 
-	samples = []byte{0x52, 0x49, 0x46, 0x46, 0x24, 0x08, 0x00, 0x00, 0x57, 0x41,
-		0x56, 0x45, 0x66, 0x6d, 0x74, 0x20, 0x10, 0x00, 0x00, 0x00,
-		0x01, 0x00, 0x02, 0x00, 0x22, 0x56, 0x00, 0x00, 0x88, 0x58,
-		0x01, 0x00, 0x04, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61,
-		0x00, 0x08, 0x00, 0x00}
+	/*
+		data = []byte{0x52, 0x49, 0x46, 0x46, 0x24, 0x08, 0x00, 0x00, 0x57, 0x41,
+			0x56, 0x45, 0x66, 0x6d, 0x74, 0x20, 0x10, 0x00, 0x00, 0x00,
+			0x01, 0x00, 0x02, 0x00, 0x22, 0x56, 0x00, 0x00, 0x88, 0x58,
+			0x01, 0x00, 0x04, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61,
+			0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0x17,
+			0x1e, 0xf3, 0x3c, 0x13, 0x3c, 0x14, 0x16, 0xf9, 0x18, 0xf9,
+			0x34, 0xe7, 0x23, 0xa6, 0x3c, 0xf2, 0x24, 0xf2, 0x11, 0xce, 0x1a, 0x0d}
+	*/
 
-	//header, err := ExtractWavHeader(data)
-	header, err := ExtractWavHeader(samples)
-
+	header, err := ExtractWavHeader(data)
 	if err != nil {
 		return err
 	}
+
 	fmt.Printf("%+v", header)
 
 	return nil
@@ -64,11 +67,15 @@ func ExtractWavHeader(headerData []byte) (*WavHeader, error) {
 		return nil, IncorrectWavFormat{}
 	}
 	header.ChunkSize = binary.LittleEndian.Uint32(headerData[4:8])
+	if int(header.ChunkSize+8) > len(headerData) {
+		return nil, IncorrectWavFormat{}
+	}
 	header.Format = string(headerData[8:12])
 	if header.Format != "WAVE" {
 		return nil, IncorrectWavFormat{}
 	}
 
+	//skip optional chunks that might be added before the fmt chunk
 	offset, err := skipChunks(headerData, 12, "fmt ")
 	if err != nil {
 		return nil, err
@@ -88,7 +95,7 @@ func ExtractWavHeader(headerData []byte) (*WavHeader, error) {
 
 	offset += 8 + int(header.Subchunk1Size)
 
-	//skip List chunk
+	//skip the optional List chunk
 	offset, err = skipChunks(headerData, offset, "data")
 	if err != nil {
 		return nil, err
@@ -96,8 +103,11 @@ func ExtractWavHeader(headerData []byte) (*WavHeader, error) {
 	header.Subchunk2ID = string(headerData[offset : offset+4])
 	header.Subchunk2Size = binary.LittleEndian.Uint32(headerData[offset+4 : offset+8])
 
-	fmt.Println(offset + 8)
+	//signify start of data
 	header.DataOffset = uint32(offset) + 8
+	if int(header.DataOffset)+int(header.Subchunk2Size) > len(headerData) {
+		return nil, IncorrectWavFormat{}
+	}
 
 	return header, nil
 }
