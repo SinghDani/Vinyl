a3c95a1738a3b66a9eb49cccfb1dbd8b26673131
Daniel Singh <singhdanield@gmail.com>
2026-03-01 21:31:56 +0100

    add methods to insert a song into the db

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -24,4 +25,28 @@ func NewDBConnection() (*DBConnection, error) {
 
 func (db *DBConnection) CloseDBConnection() {
 	db.db.Close()
+}
+
+func (db *DBConnection) ContainsSong(song string) (bool, error) {
+	var exists int
+	row := db.db.QueryRow("SELECT 1 FROM songs WHERE songname = $1", song)
+	if err := row.Scan(&exists); err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (db *DBConnection) StoreSong(songname string) error {
+	contains, err := db.ContainsSong(songname)
+	if contains {
+		return errors.New("song already in db")
+	} else if err != nil {
+		return err
+	}
+
+	_, err = db.db.Exec("INSERT INTO songs (songname) VALUES($1)", songname)
+	return err
 }
