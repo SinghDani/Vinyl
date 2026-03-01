355347b0031c42effec515934e534a2fd5c7ef2f
Daniel Singh <singhdanield@gmail.com>
2026-03-01 18:11:15 +0100

    add db connection

---
+++ db.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+type DBConnection struct {
+	DB *sql.DB
+}
+
+func NewDBConnection() (*DBConnection, error) {
+	db, err := sql.Open("pgx", os.Getenv("CONNECTIONSTRING"))
+	if err != nil {
+		return nil, err
+	}
+
+	if db.Ping() != nil {
+		return nil, err
+	}
+
+	return &DBConnection{db}, nil
+}
