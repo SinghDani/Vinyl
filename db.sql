597f5283ff7bc86ea256f5a7711f9208755c0adf
Daniel Singh <singhdanield@gmail.com>
2026-03-02 19:06:40 +0100

    db scheme for hashes

--- db.sql
+++ db.sql
@@ -2,7 +2,6 @@ create table songs (
     id serial primary key,
     name text unique not null
 );
-
 create table hashes (
     hash integer not null,
     song_id integer not null references songs(id) on delete cascade,
