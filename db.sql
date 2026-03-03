1a885f13cf272a8337efaddc52c0f7b5ae112c22
Daniel Singh <singhdanield@gmail.com>
2026-03-03 18:08:02 +0100

    rename db scheme for hashes to fingerprints

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ create table songs (
     id serial primary key,
     name text unique not null
 );
-create table hashes (
+create table fingerprints (
     hash integer not null,
     song_id integer not null references songs(id) on delete cascade,
     anchor_time integer not null
