1a267282fa1265d3b71e7b0b6d70e0dc8c3700f7
Daniel Singh <singhdanield@gmail.com>
2026-03-06 20:36:13 +0100

    add function to record audio using ffmpeg

---
+++ recording.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+)
+
+func recordAudio(outPath string, seconds int) error {
+	cmd := exec.Command(
+		"ffmpeg",
+		"-y",
+		"-f", "avfoundation",
+		"-i", ":0",
+		"-t", strconv.Itoa(seconds),
+		"-ac", "2",
+		"-ar", "44100",
+		"-c:a", "pcm_s16le",
+		outPath,
+	)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(out))
+		return err
+	}
+	return nil
+}
