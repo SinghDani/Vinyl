ece5a968eff2011745adc55aad03d63cdb87ec23
Daniel Singh <singhdanield@gmail.com>
2026-03-11 17:21:28 +0100

    function to test with csv

--- temp.go
+++ temp.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func exportAllHistogramsToCSV(timedMatches map[deltaKey]int, filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error creating CSV:", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"song_id", "dt", "count"})
+
+	for key, count := range timedMatches {
+		if count > 1 {
+			row := []string{
+				strconv.FormatUint(uint64(key.song_id), 10),
+				strconv.FormatInt(key.dt, 10),
+				strconv.Itoa(count),
+			}
+			writer.Write(row)
+		}
+	}
+	fmt.Println("Saved full histogram to", filename)
+}
+
 /*
 	//signal generation
 	N := 1024
