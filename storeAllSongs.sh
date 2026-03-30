ecc65076dcf302cd7fffc59fc638dd970b4271c1
Daniel Singh <singhdanield@gmail.com>
2026-03-30 21:44:40 +0200

    script update to work in docker

--- storeAllSongs.sh
+++ storeAllSongs.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
+##!/bin/bash
 shopt -s nullglob
-go build -o fingerprinting
+#go build -o fingerprinting
 for file in audioFiles/*.wav; do
     echo "$file"
-    ./fingerprinting 1 "$file"
+    #./fingerprinting 1 "$file"
+    ./audioRec 1 "$file"
 done
 
-rm fingerprinting
+#rm fingerprinting
