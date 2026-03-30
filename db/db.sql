81a50033ccd923c71695d4d5fed95e138a7e9bd1
Daniel Singh <singhdanield@gmail.com>
2026-03-30 21:43:47 +0200

    updates to db schema

--- db/db.sql
+++ db/db.sql
@@ -1,8 +1,8 @@
-create table songs (
+create table if not exists songs (
     id serial primary key,
     name text unique not null
 );
-create table fingerprints (
+create table if not exists fingerprints (
     hash integer not null,
     song_id integer not null references songs(id) on delete cascade,
     anchor_time integer not null
