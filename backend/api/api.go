fb41c0cbddf673fdef19f104157f1d6f103c567d
Daniel Singh <singhdanield@gmail.com>
2026-04-12 16:54:01 +0200

    add home route

--- backend/api/api.go
+++ backend/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -39,4 +40,20 @@ func (s *Server) Run() {
 func (s *Server) registerRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/", s.home)
 	mux.HandleFunc("/recording", s.acceptRecording)
+}
+
+func (s *Server) home(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	songs, err := s.Db.GetAllSongs()
+	if err != nil {
+		http.Error(w, "db access failed", http.StatusInternalServerError)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(songs); err != nil {
+		http.Error(w, "could not send songs", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *Server) acceptRecording(w http.ResponseWriter, r *http.Request) {
 }
