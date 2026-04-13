d60fb95052310856d7150bb138c508dee30aeb4f
Daniel Singh <singhdanield@gmail.com>
2026-04-13 13:16:03 +0200

    add recording functionality to backend socket connection

--- backend/api/api.go
+++ backend/api/api.go
@@ -2,10 +2,15 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/SinghDani/audioRecognition/db"
+	"github.com/SinghDani/audioRecognition/fingerprint"
+	"github.com/SinghDani/audioRecognition/internal"
+	"github.com/SinghDani/audioRecognition/wav"
 	"github.com/gorilla/websocket"
 )
 
@@ -56,4 +61,90 @@ func (s *Server) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) acceptRecording(w http.ResponseWriter, r *http.Request) {
+	conn, err := s.WsUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("upgrade failed:", err)
+		return
+	}
+
+	type match struct {
+		SongName string `json:"songName"`
+		Verdict  string `json:"verdict"`
+	}
+
+	totalTime := 40     // record for max of 40 sec
+	totalBatchTime := 5 // process in batches of 5 sec
+	windowsPerBatch := fingerprint.SecondsToWindows(float64(totalBatchTime))
+	samplesPerBatch := totalBatchTime * fingerprint.SamplingRate
+
+	var masterHashList [][]internal.GeneratedHash
+	var matchingSong internal.MatchingSong
+	buffer := make([]float64, 0, samplesPerBatch*2)
+	chunkIndex := 0
+
+	defer conn.Close()
+
+	for chunkIndex*totalBatchTime < totalTime {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("ws read failed:", err)
+			return
+		}
+
+		samples, err := wav.BytesToSamples(p)
+		if err != nil {
+			fmt.Println("invalid audio payload:", err)
+			closeMsg := websocket.FormatCloseMessage(websocket.CloseUnsupportedData, err.Error())
+			_ = conn.WriteControl(websocket.CloseMessage, closeMsg, time.Now().Add(time.Second))
+			return
+		}
+
+		buffer = append(buffer, samples...)
+		for len(buffer) >= samplesPerBatch {
+			chunkSamples := make([]float64, samplesPerBatch)
+			copy(chunkSamples, buffer[:samplesPerBatch])
+			buffer = buffer[samplesPerBatch:]
+
+			timeOffset := chunkIndex * windowsPerBatch
+			chunkIndex++
+			genHashes, err := fingerprint.SamplesToHashes(chunkSamples, timeOffset)
+			if err != nil {
+				fmt.Println("hash extraction failed:", err)
+				closeMsg := websocket.FormatCloseMessage(websocket.CloseInternalServerErr, err.Error())
+				_ = conn.WriteControl(websocket.CloseMessage, closeMsg, time.Now().Add(time.Second))
+				return
+			}
+
+			masterHashList = append(masterHashList, genHashes)
+
+			batchWindow := 3 //only look at last 15 seconds
+			var hashBatch []internal.GeneratedHash
+
+			start := max(0, len(masterHashList)-batchWindow)
+			for _, batch := range masterHashList[start:] {
+				hashBatch = append(hashBatch, batch...)
+			}
+
+			matchingSong, err = fingerprint.IdentifyRecording(s.Db, hashBatch)
+			if err != nil {
+				fmt.Println("identify failed:", err)
+				closeMsg := websocket.FormatCloseMessage(websocket.CloseInternalServerErr, err.Error())
+				_ = conn.WriteControl(websocket.CloseMessage, closeMsg, time.Now().Add(time.Second))
+				return
+			}
+
+			if fingerprint.EvalMatch(matchingSong) {
+				fingerprint.PrintVerdict(matchingSong, s.Db)
+				if err := conn.WriteJSON(match{matchingSong.SongName, fingerprint.GetVerdict(matchingSong)}); err != nil {
+					fmt.Println("ws write winner failed:", err)
+				}
+				return
+			}
+		}
+	}
+
+	fingerprint.PrintVerdict(matchingSong, s.Db)
+	if err := conn.WriteJSON(match{matchingSong.SongName, fingerprint.GetVerdict(matchingSong)}); err != nil {
+		fmt.Println("ws write final result failed:", err)
+	}
 }
