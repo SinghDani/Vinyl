8deaf4efa637ea7d56929a5ab923578eb3339925
Daniel Singh <singhdanield@gmail.com>
2026-09-24 15:09:50 -0300

    db now uses pgx with context

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,18 +1,19 @@
 package db
 
 import (
-	"database/sql"
+	"context"
 	"errors"
 	"fmt"
 	"os"
 	"regexp"
 
 	"github.com/SinghDani/audioRecognition/internal"
-	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type DBConnection struct {
-	db *sql.DB
+	db *pgxpool.Pool
 }
 
 func NewDBConnection() (*DBConnection, error) {
@@ -25,36 +26,37 @@ func NewDBConnection() (*DBConnection, error) {
 		os.Getenv("DB_SSLMODE"),
 	)
 
-	db, err := sql.Open("pgx", connection)
+	ctx := context.Background()
+	db, err := pgxpool.New(ctx, connection)
 	if err != nil {
 		return nil, err
 	}
-	if err := db.Ping(); err != nil {
+	if err := db.Ping(ctx); err != nil {
 		return nil, err
 	}
 	return &DBConnection{db}, nil
 }
 
-func (db *DBConnection) CloseDBConnection() error {
-	return db.db.Close()
+func (db *DBConnection) CloseDBConnection() {
+	db.db.Close()
 }
 
-func (db *DBConnection) ContainsSong(songname string) (bool, error) {
+func (db *DBConnection) ContainsSong(ctx context.Context, songname string) (bool, error) {
 	var exists bool
-	row := db.db.QueryRow("SELECT EXISTS (SELECT 1 FROM songs WHERE name = $1)", songname)
+	row := db.db.QueryRow(ctx, "SELECT EXISTS (SELECT 1 FROM songs WHERE name = $1)", songname)
 	if err := row.Scan(&exists); err != nil {
 		return false, err
 	}
 	return exists, nil
 }
 
-func (db *DBConnection) RemoveSong(songname string) error {
-	_, err := db.db.Exec("DELETE FROM songs WHERE name = $1", songname)
+func (db *DBConnection) RemoveSong(ctx context.Context, songname string) error {
+	_, err := db.db.Exec(ctx, "DELETE FROM songs WHERE name = $1", songname)
 	return err
 }
 
-func (db *DBConnection) GetSong(songId uint32) (internal.Song, error) {
-	row := db.db.QueryRow("SELECT name, artist FROM songs WHERE id = $1", int32(songId))
+func (db *DBConnection) GetSong(ctx context.Context, songId uint32) (internal.Song, error) {
+	row := db.db.QueryRow(ctx, "SELECT name, artist FROM songs WHERE id = $1", int32(songId))
 	var songName string
 	var artist string
 	if err := row.Scan(&songName, &artist); err != nil {
@@ -63,14 +65,14 @@ func (db *DBConnection) GetSong(songId uint32) (internal.Song, error) {
 	return internal.Song{Name: songName, Artist: artist}, nil
 }
 
-func (db *DBConnection) GetAllSongs() ([]internal.Song, error) {
-	rows, err := db.db.Query("SELECT name, artist FROM songs")
+func (db *DBConnection) GetAllSongs(ctx context.Context) ([]internal.Song, error) {
+	rows, err := db.db.Query(ctx, "SELECT name, artist FROM songs")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var songs []internal.Song
+	songs := make([]internal.Song, 0)
 	var songName string
 	var artist string
 	for rows.Next() {
@@ -82,7 +84,7 @@ func (db *DBConnection) GetAllSongs() ([]internal.Song, error) {
 	return songs, nil
 }
 
-func (db *DBConnection) StoreHashes(hashes []internal.GeneratedHash, song string) error {
+func (db *DBConnection) StoreHashes(ctx context.Context, hashes []internal.GeneratedHash, song string) error {
 	if len(hashes) == 0 {
 		return errors.New("no hashes to store")
 	}
@@ -94,45 +96,44 @@ func (db *DBConnection) StoreHashes(hashes []internal.GeneratedHash, song string
 		return errors.New("wrong file format")
 	}
 
-	tx, err := db.db.Begin()
+	tx, err := db.db.Begin(ctx)
 	if err != nil {
 		return err
 	}
-	defer tx.Rollback()
+	defer tx.Rollback(ctx)
 
 	var id int32
 	//since name is unique will return err if song already in db
-	row := tx.QueryRow("INSERT INTO songs (name, artist) VALUES($1, $2) RETURNING id", songInfo[1], songInfo[2])
+	row := tx.QueryRow(ctx, "INSERT INTO songs (name, artist) VALUES($1, $2) RETURNING id", songInfo[1], songInfo[2])
 	if err := row.Scan(&id); err != nil {
 		return err
 	}
 
-	stmt, err := tx.Prepare("INSERT INTO fingerprints (hash, song_id, anchor_time) VALUES($1, $2, $3)")
+	_, err = tx.CopyFrom(ctx,
+		pgx.Identifier{"fingerprints"},
+		[]string{"hash", "song_id", "anchor_time"},
+		pgx.CopyFromSlice(len(hashes), func(i int) ([]any, error) {
+			return []any{int32(hashes[i].Hash), id, int32(hashes[i].AnchorTime)}, nil
+		}),
+	)
 	if err != nil {
 		return err
 	}
-	defer stmt.Close()
 
-	for _, hash := range hashes {
-		if _, err := stmt.Exec(int32(hash.Hash), id, int32(hash.AnchorTime)); err != nil {
-			return err
-		}
-	}
-
-	return tx.Commit()
+	return tx.Commit(ctx)
 }
 
-func (db *DBConnection) ExtractHashes(hashes []uint32) ([]internal.Fingerprint, error) {
+func (db *DBConnection) ExtractHashes(ctx context.Context, hashes []internal.GeneratedHash) ([]internal.Fingerprint, error) {
 	if len(hashes) == 0 {
 		return nil, nil
 	}
 	intHashes := make([]int32, len(hashes))
 	for i, h := range hashes {
-		intHashes[i] = int32(h)
+		intHashes[i] = int32(h.Hash)
 	}
 
 	var matches []internal.Fingerprint
-	rows, err := db.db.Query("SELECT hash, song_id, anchor_time from fingerprints WHERE hash = ANY($1)", intHashes)
+	rows, err := db.db.Query(ctx, "SELECT hash, song_id, anchor_time from fingerprints WHERE hash = ANY($1)", intHashes)
 	if err != nil {
 		return nil, err
 	}
